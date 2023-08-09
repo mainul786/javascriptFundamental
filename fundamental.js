fc4c5efcc9b197deec22f66068dfb03e6940ac9e
mainul96 <110577655+mainul96@users.noreply.github.com>
2023-08-09 21:43:27 +0530

    first commit

---
+++ fundamental.js
@@ -0,0 +1,70 @@
+// veriable decalare process
+var price = 20;
+var bookName = 'javaScript';
+var isMoney = false;
+var name;
+var age = null;
+
+// mathemetical operation
+// addition 
+var applePrice = 40;
+var orangePrice = 60;
+var totalPrice = applePrice + orangePrice;
+console.log(totalPrice);
+
+// substraction 
+var applePrice = 40;
+var orangePrice = 60;
+var totalPrice = applePrice - orangePrice;
+console.log(totalPrice);
+
+// multiplication
+
+var applePrice = 40;
+var orangePrice = 60;
+var totalPrice = applePrice * orangePrice;
+console.log(totalPrice);
+
+// divition
+
+var applePrice = 40;
+var orangePrice = 2;
+var totalPrice = applePrice / orangePrice;
+console.log(totalPrice);
+
+// two string concatenate 
+var fistName = 'Mianul';
+var lastName = 'islam';
+var fullName = fistName + ' ' + lastName;
+console.log(fullName);
+
+// shortHand
+var price = 10;
+// price = price + 20;
+price += 20;
+console.log(price);
+
+price++;
+console.log(price);
+
+price--;
+console.log(price);
+
+// string convert to int
+var a = '10';
+console.log(parseInt(a));
+
+var b = '3.10';
+console.log(parseFloat(b));
+
+var a = 0.1;
+var b = 0.2;
+var total = a + b;
+console.log(parseFloat(total.toFixed(1)));
+
+// modulus 
+var a = 22;
+var b = 10;
+var result = a % b;
+console.log(result);
+
